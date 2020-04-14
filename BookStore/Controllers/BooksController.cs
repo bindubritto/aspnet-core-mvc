a8c3ca471f005a6a88abcae409dbc39af1b475d2
sabbir-bs <Sabbir.ah@brainstation23.com>
2020-04-14 11:45:20 +0600

    ASP.NET Core MVC Project

---
+++ BookStore/Controllers/BooksController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BookStore.Controllers
+{
+    public class BooksController : Controller
+    {
+        // GET: /<controller>/
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+
+            Book book = new Book()
+            {
+                Id = 1,
+                Title = "Learning ASP.NET Core 2.0",
+                Genre = "Programming & Software Development",
+                Price = 45,
+                PublishDate = new System.DateTime(2012, 04, 23),
+                Authors = new List<string> { "Jason De Oliveira", "Michel Bruchet" }
+            };
+            return View(book);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Book book)
+        {
+            if(ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(book);
+        }
+    }
+}
